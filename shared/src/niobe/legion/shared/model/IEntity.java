fd10aa70dd19e61bc8351031d4246d9b9a4536d3
fireandfuel <fireandfuel@hotmail.de>
2015-12-18 11:28:26 +0100

    Added copyright notice

--- shared/src/niobe/legion/shared/model/IEntity.java
+++ shared/src/niobe/legion/shared/model/IEntity.java
@@ -1,3 +1,23 @@
+/*
+ * Niobe Legion - a versatile client / server framework
+ *     Copyright (C) 2013-2015 by fireandfuel (fireandfuel<at>hotmail<dot>de)
+ *
+ * This file (IEntity.java) is part of Niobe Legion (module niobe-legion-shared).
+ *
+ *     Niobe Legion is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Niobe Legion is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with Niobe Legion.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package niobe.legion.shared.model;
 
 import javax.persistence.Access;
--- shared/src/niobe/legion/shared/sasl/AnonymousSaslClient.java
+++ shared/src/niobe/legion/shared/sasl/AnonymousSaslClient.java
@@ -1,25 +1,21 @@
 /*
- * The MIT License (MIT)
+ * Niobe Legion - a versatile client / server framework
+ *     Copyright (C) 2013-2015 by fireandfuel (fireandfuel<at>hotmail<dot>de)
  *
- * Copyright (c) 2014 Christian Schudt
+ * This file (AnonymousSaslClient.java) is part of Niobe Legion (module niobe-legion-shared).
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *     Niobe Legion is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ *     Niobe Legion is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with Niobe Legion.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package niobe.legion.shared.sasl;
